28074422b9f531e3ea891ee74a49338675244178
carbondal <allan.porter@state.co.us>
2023-01-25 14:03:45 -0700

    update pandas code

--- camera_scraper.py
+++ camera_scraper.py
@@ -19,55 +19,68 @@ img_type = "jpg" # File extention used when saving image files
 img_dir = "./camera" # Directory where subdirectories for each camera will be stored
 capture_interval = 10 # Interval between end of last capture and start of current capture
 
+
+#this works and creates a menu of info per item (see screenshot)
+# df = pd.read_csv('MtnCameras.csv')
+# for i, j in df.iterrows():
+#     print(i, j)
+#     print()
+
+#This works and creats an array with Index #s)
 df = pd.read_csv('MtnCameras.csv')
-for i ,row in df.iterrows():
-        value = print(f"{row['url']}")
+for row in df.itertuples():
+    print(row)
 
-def capture_all_cameras():
-    currentTimeDate = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    print(f"Capture at {currentTimeDate}")
-    for camera in camera_ids:
-        # Combines the URL
-        url = f'{url_front}{camera}{url_end}'
-        try:
-            r = requests.get(url, allow_redirects=True)
-            open(f'{img_dir}/{camera}/camera{camera}-{currentTimeDate}.{img_type}', 'wb').write(r.content)
-            status = "success"
-        except requests.ConnectionError:
-            status = "ERROR (network)"
-        except OSError:
-            status = "ERROR (file)"
-        except:
-            status = "ERROR (other)"
-        print(f"\t{camera}  \t\t {status}")
+#this csv works but doesn't iterate through rows, saving, naming and uploading images to s3.
+# df = pd.read_csv('MtnCameras.csv')
+# for i ,row in df.iterrows():
+#         value = print(f"{row['url_front']}")
 
-
-def configure_folders():
-     for camera in camera_ids:
-        path = f'{img_dir}/{camera}'
-        pathExists = os.path.exists(path)
-        if not pathExists:
-            print(f"Creating path for camera {camera}")
-            os.makedirs(path)
-        else:
-             print(f"Path for camera {camera} exists")
-
-def files():
-    list_of_files = glob.glob(path + '*.jpg')
-    latest_file = max(list_of_files, key=os.path.getctime)
-    print(latest_file)
-
-    s3 = boto3.client('s3')
-    with open(latest_file, 'rb') as f:
-        s3.upload_fileobj(f, S3_BUCKET_NAME, latest_file)
+# def capture_all_cameras():
+#     currentTimeDate = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+#     print(f"Capture at {currentTimeDate}")
+#     for camera in camera_ids:
+#         # Combines the URL
+#         url = f'{url_front}{camera}{url_end}'
+#         try:
+#             r = requests.get(url, allow_redirects=True)
+#             open(f'{img_dir}/{camera}/camera{camera}-{currentTimeDate}.{img_type}', 'wb').write(r.content)
+#             status = "success"
+#         except requests.ConnectionError:
+#             status = "ERROR (network)"
+#         except OSError:
+#             status = "ERROR (file)"
+#         except:
+#             status = "ERROR (other)"
+#         print(f"\t{camera}  \t\t {status}")
 
 
-camera_ids.sort()
-configure_folders()
-while(True):
-    capture_all_cameras()
-    time.sleep(capture_interval)
-    files()
+# def configure_folders():
+#      for camera in camera_ids:
+#         path = f'{img_dir}/{camera}'
+#         pathExists = os.path.exists(path)
+#         if not pathExists:
+#             print(f"Creating path for camera {camera}")
+#             os.makedirs(path)
+#         else:
+#              print(f"Path for camera {camera} exists")
+
+# def files():
+#     list_of_files = glob.glob(path + '*.jpg')
+#     latest_file = max(list_of_files, key=os.path.getctime)
+#     print(latest_file)
+
+#     s3 = boto3.client('s3')
+#     with open(latest_file, 'rb') as f:
+#         s3.upload_fileobj(f, S3_BUCKET_NAME, latest_file)
+
+
+# camera_ids.sort()
+# configure_folders()
+# while(True):
+#     capture_all_cameras()
+#     time.sleep(capture_interval)
+#     files()
 
 
 
